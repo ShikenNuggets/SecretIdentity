622bafc939249142dc4f5f2b0028ca85013336c6
ShikenNuggets <carter.rennick@gmail.com>
2024-05-31 18:10:34 -0400

    Updated Target.cs for correct final .exe name

--- Source/SecretIdentity.Target.cs
+++ Source/SecretIdentity.Target.cs
@@ -2,14 +2,15 @@
 
 using UnrealBuildTool;
 
-public class CitySampleTarget : TargetRules
+public class SecretIdentityTarget : TargetRules
 {
-	public CitySampleTarget(TargetInfo Target) : base(Target)
+	public SecretIdentityTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
+		ExtraModuleNames.Add("SecretIdentity");
 		ExtraModuleNames.Add("CitySample");
 
 		if (BuildEnvironment == TargetBuildEnvironment.Unique)
