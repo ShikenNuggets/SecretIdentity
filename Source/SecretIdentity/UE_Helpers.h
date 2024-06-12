370d920e360cdedfde09fdce332aa11425aa0133
ShikenNuggets <carter.rennick@gmail.com>
2024-06-11 20:22:15 -0400

    Prevented possible dangling else bugs

--- Source/SecretIdentity/UE_Helpers.h
+++ Source/SecretIdentity/UE_Helpers.h
@@ -9,27 +9,27 @@
 #endif //!__FILE_LINE_FSTRING__
 
 #ifndef LOG_MSG
-	#define LOG_MSG(M) if (GEngine) { GEngine->AddOnScreenDebugMessage(INDEX_NONE, 30.0f, FColor::Green, M + __FILE_LINE_FSTRING__, false); }
+	#define LOG_MSG(M) if (GEngine) { GEngine->AddOnScreenDebugMessage(INDEX_NONE, 30.0f, FColor::Green, M + __FILE_LINE_FSTRING__, false); }else{}
 #endif //!LOG_MSG
 
 #ifndef LOG_MSG_WARNING
-	#define LOG_MSG_WARNING(M) if (GEngine) { GEngine->AddOnScreenDebugMessage(INDEX_NONE, 30.0f, FColor::Yellow, M + __FILE_LINE_FSTRING__, false); }
+	#define LOG_MSG_WARNING(M) if (GEngine) { GEngine->AddOnScreenDebugMessage(INDEX_NONE, 30.0f, FColor::Yellow, M + __FILE_LINE_FSTRING__, false); }else{}
 #endif // !LOG_MSG_WARNING
 
 #ifndef LOG_MSG_ERROR
-	#define LOG_MSG_ERROR(M) if (GEngine) { GEngine->AddOnScreenDebugMessage(INDEX_NONE, 30.0f, FColor::Red, M + __FILE_LINE_FSTRING__, false); }
+	#define LOG_MSG_ERROR(M) if (GEngine) { GEngine->AddOnScreenDebugMessage(INDEX_NONE, 30.0f, FColor::Red, M + __FILE_LINE_FSTRING__, false); }else{}
 #endif //!LOG_MSG
 
 #ifndef WARN_IF
-	#define WARN_IF(T) if (T && GEngine) { GEngine->AddOnScreenDebugMessage(INDEX_NONE, 30.0f, FColor::Red, TEXT(#T " was true" + __FILE_LINE_FSTRING__)); }
+	#define WARN_IF(T) if (T && GEngine) { GEngine->AddOnScreenDebugMessage(INDEX_NONE, 30.0f, FColor::Red, TEXT(#T " was true" + __FILE_LINE_FSTRING__)); }else{}
 #endif // !WARN_IF
 
 #ifndef WARN_IF_MSG
-	#define WARN_IF_MSG(T,M) if (T && GEngine) { GEngine->AddOnScreenDebugMessage(INDEX_NONE, 30.0f, FColor::Red, TEXT(M + __FILE_LINE_FSTRING__), false); }
+	#define WARN_IF_MSG(T,M) if (T && GEngine) { GEngine->AddOnScreenDebugMessage(INDEX_NONE, 30.0f, FColor::Red, TEXT(M + __FILE_LINE_FSTRING__), false); }else{}
 #endif // !WARN_IF_MSG
 
 #ifndef WARN_IF_NULL
-	#define WARN_IF_NULL(T) if (!T && GEngine) { GEngine->AddOnScreenDebugMessage(INDEX_NONE, 30.0f, FColor::Red, TEXT("Required field " #T " was null" + __FILE_LINE_FSTRING__), false); }
+	#define WARN_IF_NULL(T) if (!T && GEngine) { GEngine->AddOnScreenDebugMessage(INDEX_NONE, 30.0f, FColor::Red, TEXT("Required field " #T " was null" + __FILE_LINE_FSTRING__), false); }else{}
 #endif // !WARN_IF_NULL
 
 /**
